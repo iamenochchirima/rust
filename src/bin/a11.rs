df52fd9463713feb36335209a71f94436b270e0a
iamenochchirima <iamenochchirima@gmail.com>
2023-06-03 23:46:31 +0200

    impl, data ownership

--- src/bin/a11.rs
+++ src/bin/a11.rs
@@ -9,4 +9,29 @@
 // * Create a function to display the quantity, with the struct as a parameter
 // * Create a function to display the id number, with the struct as a parameter
 
-fn main() {}
+struct Grocery {
+    id: i32,
+    name: String,
+    price: i32,
+    quantity: i32,
+}
+
+fn item_quantity(item: &Grocery) {
+    println!("Quantity: {:?}", item.quantity)
+}
+fn item_id(item: &Grocery) {
+    println!("Identity: {:?}", item.id)
+}
+
+fn main() {
+    let item = Grocery {
+        id: 1,
+        quantity: 3,
+        name: ("bananas").to_string(),
+        price: 2,
+    };
+    item_quantity(&item);
+    item_id(&item);
+
+
+}
---
+++ src/bin/impl.rs
@@ -0,0 +1,71 @@
+// Topic: Implementing functionality with the impl keyword
+//
+// Requirements:
+// * Print the characteristics of a shipping box
+// * Must include dimensions, weight, and color
+//
+// Notes:
+// * Use a struct to encapsulate the box characteristics
+// * Use an enum for the box color
+// * Implement functionality on the box struct to create a new box
+// * Implement functionality on the box struct to print the characteristics
+
+enum Color {
+    Blue,
+    White,
+}
+
+impl Color {
+    fn print(&self) {
+        match self {
+            Color::Blue => println!("brown"),
+            Color::White => println!("white"),
+        }
+    }
+}
+
+struct Dimensions {
+    length: f64,
+    width: f64,
+    height: f64,
+}
+
+impl Dimensions {
+    fn print(&self) {
+        println!("Length: {:?}", self.length);
+        println!("Width: {:?}", self.width);
+        println!("Height: {:?}", self.height);
+    }
+}
+struct ShippingBox {
+    dimensions: Dimensions,
+    weight: f64,
+    color: Color,
+}
+
+impl ShippingBox {
+    fn new_box(weight: f64, color: Color, dimensions: Dimensions) -> Self {
+        Self {
+            dimensions: dimensions,
+            weight: weight,
+            color : color,
+        }
+    }
+
+    fn print(&self) {
+        self.color.print();
+        self.dimensions.print();
+        println!("Weight: {:?}", self.weight)
+    }
+
+}
+
+fn main() {
+   let small_dimensions = Dimensions {
+    length: 4.0,
+    width: 2.5,
+    height: 2.5,
+   };
+   let small_box = ShippingBox::new_box(3.3, Color::Blue,small_dimensions);
+   small_box.print()
+}
