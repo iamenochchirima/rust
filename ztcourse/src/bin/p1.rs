22d273b2e9bb48aad6803582a32267b527c7cfc6
iamenochchirima <iamenochchirima@gmail.com>
2024-01-21 19:51:35 +0200

    Add bill, view bills

--- ztcourse/src/bin/p1.rs
+++ ztcourse/src/bin/p1.rs
@@ -29,16 +29,30 @@
 // * A vector is the easiest way to store the bills at stage 1, but a
 //   hashmap will be easier to work with at stages 2 and 3.
 
-use std::collections::HashMap;
 use std::io;
-use uuid::Uuid;
 
-
-
-struct Bill {
-    id: String,
+#[derive(Debug, Clone)]
+pub struct Bill {
     name: String,
     amount: f64,
+}
+
+pub struct Bills {
+    bills: Vec<Bill>,
+}
+
+impl Bills {
+    fn new() -> Self {
+        Self { bills: Vec::new() }
+    }
+
+    fn add(&mut self, bill: Bill) {
+        self.bills.push(bill);
+    }
+
+    fn get_all(&self) -> Vec<&Bill> {
+        self.bills.iter().collect()
+    }
 }
 
 fn get_input() -> Option<String> {
@@ -53,6 +67,28 @@ fn get_input() -> Option<String> {
         return None;
     } else {
         return Some(input);
+    }
+}
+
+fn get_bill_amount() -> Option<f64> {
+    println!("Bill amount:");
+
+    loop {
+        let input = match get_input() {
+            Some(input) => input,
+            None => return None,
+        };
+        if &input == "" {
+            println!("Input cannot be empty, please try again");
+            return None;
+        }
+        let parsed_input: Result<f64, _> = input.parse();
+        match parsed_input {
+            Ok(amount) => return Some(amount),
+            Err(_) => {
+                println!("Invalid amount, please try again");
+            }
+        }
     }
 }
 
@@ -78,28 +114,64 @@ impl MainMenu {
         println!("2. View Bills");
         println!("");
         println!("Please choose an option:");
-
     }
 }
 
+mod menu {
+    use crate::{get_input , get_bill_amount, Bill, Bills};
 
+    pub fn add_bill(bills: &mut Bills) {
+        println!("Bill name:");
+        let name = match get_input() {
+            Some(name) => name,
+            None => {
+                println!("Invalid name");
+                return;
+            }
+        };
+        let amount = match get_bill_amount() {
+            Some(amount) => amount,
+            None => {
+                println!("Invalid amount");
+                return;
+            }
+        };
+        let bill = Bill {
+            name,
+            amount,
+        };
+        bills.add(bill);
+        println!("Bill added");
+    }
+
+    pub fn view_bills(bills: &Bills) {
+        println!("Bills:");
+        for bill in bills.get_all() {
+            println!("Bill name-{}: Ammount-${}", bill.name, bill.amount);
+        }
+    }
+}
 
 fn main() {
-    
+
+    let mut bills = Bills::new();
+
     loop {
         MainMenu::show();
         let input = get_input().expect("No data entered");
         match MainMenu::from_str(&input.as_str()) {
             Some(MainMenu::AddBill) => {
                 println!("Add Bill");
-            },
+                menu::add_bill(&mut bills);
+            }
             Some(MainMenu::ViewBills) => {
                 println!("View Bills");
-            },
+                menu::view_bills(&bills);
+            }
             None => {
                 println!("Invalid option");
                 return;
-            },
+            }
         }
     }
 }
