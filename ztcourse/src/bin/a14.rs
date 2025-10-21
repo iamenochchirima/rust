0878677a1284b9d00139f4b0ad9580e34715242c
iamenochchirima <iamenochchirima@gmail.com>
2025-10-21 17:50:41 +0200

    Implement Person struct and display logic for people aged 10 and under

--- ztcourse/src/bin/a14.rs
+++ ztcourse/src/bin/a14.rs
@@ -11,5 +11,43 @@
 // * Use an if expression to determine which person's info should be printed
 // * The name and colors should be printed using a function
 
-fn main() {}
+
+struct Person {
+    name : String,
+    age : u8,
+    color : String
+}
+
+impl Person {
+    fn new(name: String, age: u8, color: String) -> Self {
+        Self {
+            name,
+            age,
+            color
+        }
+    }
+
+    fn print_name_and_color(&self) {
+        println!("Name : {:?}, Color : {:?}", self.name, self.color)
+    }
+ }
+
+fn main() {
+
+    let person1 = Person::new("Tadi".to_owned(), 16, "Green".to_owned());
+    let person2 = Person::new("Enoch".to_owned(), 6, "Purple".to_owned());
+    let person3 = Person::new("Wande".to_owned(), 4, "Green".to_owned());
+
+    let mut people: Vec<Person>  = Vec::new();
+    people.push(person1);
+    people.push(person2);
+    people.push(person3);
+
+    for person in people {
+        if person.age < 10 {
+            println!("Age : {:?}", person.age);
+            person.print_name_and_color();
+        }
+    }
+}
 
--- ztcourse/src/bin/a15.rs
+++ ztcourse/src/bin/a15.rs
@@ -11,5 +11,44 @@
 // * Create one of each ticket and place into a vector
 // * Use a match expression while iterating the vector to print the ticket info
 
-fn main() {}
+#[derive(Debug)]
+enum TicketType {
+    Standard,
+    Backstage(String),
+    Vip(String)
+}
+
+#[derive(Debug)]
+struct Ticket {
+    ticket_tier : TicketType,
+    price : u64,
+}
+
+impl Ticket {
+    fn new(tier : TicketType, price: u64) -> Self {
+        Self {
+            ticket_tier : tier,
+            price
+        }
+    }
+}
+
+fn main() {
+    let ticket_1 = Ticket::new(TicketType::Standard, 64);
+    let ticket_2 = Ticket::new(TicketType::Backstage("Wande".to_owned()), 100);
+    let ticket_3 = Ticket::new(TicketType::Vip("Enoch".to_owned()), 150);
+
+    let mut tickets : Vec<Ticket> = Vec::new();
+    tickets.push(ticket_1);
+    tickets.push(ticket_2);
+    tickets.push(ticket_3);
+
+    for ticket in tickets {
+        match ticket.ticket_tier {
+            TicketType::Standard => println!("Standard  TicketPrice : {:?}", ticket.price),
+            TicketType::Backstage(holder) => println!("Backstage ticket for : {:?}, Price : {:?}", holder, ticket.price),
+            TicketType::Vip(holder) => println!("Vip ticket for : {:?}Price : {:?}", holder, ticket.price)
+        }
+    }
+}
 
