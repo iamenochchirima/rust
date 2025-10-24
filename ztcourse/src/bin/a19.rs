b70bb926bc4ca7a06d19d5b60daede10f7daf409
iamenochchirima <iamenochchirima@gmail.com>
2025-10-24 21:12:09 +0200

    Implement furniture store stock management with HashMap and display logic

--- ztcourse/src/bin/a19.rs
+++ ztcourse/src/bin/a19.rs
@@ -13,5 +13,26 @@
 // Notes:
 // * Use a HashMap for the furniture store stock
 
-fn main() {}
+use std::collections::HashMap;
 
+struct Stock {
+    num_items: u32,
+    name: String,
+}
+
+fn main() {
+    let mut stocks: HashMap<u8, Stock> = HashMap::new();
+    stocks.insert(0, Stock { num_items: 5, name: "Chairs".to_owned() });
+    stocks.insert(1, Stock { num_items: 3, name: "Beds".to_owned() });
+    stocks.insert(2, Stock { num_items: 2, name: "Tables".to_owned() });
+    stocks.insert(3, Stock { num_items: 0, name: "Couches".to_owned() });
+
+    for stock in stocks.values() {
+        match stock.num_items {
+            0 => {
+                println!("{:?} is out of stock", stock.name);
+            }
+            _ => println!("{:?} items available of : {:?}", stock.num_items, stock.name)
+        }
+    }
+}
